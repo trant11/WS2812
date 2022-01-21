50b2641c81f6a3f9d5cd30aaa5f25d06c3a5ac18
trant11 <tsmida703@seznam.cz>
2022-01-21 03:45:50 +0100

    ENCODER error

--- src/main.c
+++ src/main.c
@@ -1,82 +1,202 @@
 #include "stm8s.h"
-#include "milis.h"
 #include "delay.h"
+#include "milis.h"
+#include "stm8_hd44780.h"
+#include "stdio.h"
 
-#define _ISOC99_SOURCE
-#define _GNU_SOURCE
+#define swt_PORT GPIOB
+#define swt_PIN GPIO_PIN_7
+#define swt (GPIO_ReadInputPin(swt_PORT, swt_PIN)== RESET)
 
+#define NCODER_CLK_PORT GPIOE
+#define NCODER_DATA_PORT GPIOE
+#define NCODER_CLK_PIN GPIO_PIN_1
+#define NCODER_DATA_PIN GPIO_PIN_2
+#define NCODER_GET_CLK (GPIO_ReadInputPin(NCODER_CLK_PORT, NCODER_CLK_PIN)!=RESET)
+#define NCODER_GET_DATA (GPIO_ReadInputPin(NCODER_DATA_PORT, NCODER_DATA_PIN)!=RESET)
 
-void init_spi(void)
-{
-    // Software slave managment (disable CS/SS input), BiDirectional-Mode release MISO pin to general purpose
-    SPI->CR2 |= SPI_CR2_SSM | SPI_CR2_SSI | SPI_CR2_BDM | SPI_CR2_BDOE;
-    // Enable SPI as master at maximum speed (F_MCU/2, there 16/2=8MHz)
-    SPI->CR1 |= SPI_CR1_SPE | SPI_CR1_MSTR;
+//ENCODER DEFINE
+/*#define swt_PORT GPIOE
+#define swt_PIN GPIO_PIN_0
+#define enc_swt (GPIO_ReadInputPin(swt_PORT, swt_PIN)== RESET)
+#define NCODER_CLK_PORT GPIOD
+#define NCODER_DATA_PORT GPIOD
+#define NCODER_CLK_PIN GPIO_PIN_6
+#define NCODER_DATA_PIN GPIO_PIN_5
+#define NCODER_GET_CLK (GPIO_ReadInputPin(NCODER_CLK_PORT, NCODER_CLK_PIN)!=RESET)
+#define NCODER_GET_DATA (GPIO_ReadInputPin(NCODER_DATA_PORT, NCODER_DATA_PIN)!=RESET)
+*/
+//LCD DEFINE
+#define LCD_RS_PORT GPIOF
+#define LCD_RW_PORT GPIOF
+#define LCD_E_PORT GPIOF
+#define LCD_D4_PORT GPIOG
+#define LCD_D5_PORT GPIOG
+#define LCD_D6_PORT GPIOG
+#define LCD_D7_PORT GPIOG
+
+#define LCD_RS_PIN GPIO_PIN_7
+#define LCD_RW_PIN GPIO_PIN_6
+#define LCD_E_PIN GPIO_PIN_5
+#define LCD_D4_PIN GPIO_PIN_0
+#define LCD_D5_PIN GPIO_PIN_1
+#define LCD_D6_PIN GPIO_PIN_2
+#define LCD_D7_PIN GPIO_PIN_3
+
+void setup(void){
+CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); //Taktovat MCU 16 MHz
+
+//GPIO SETUP
+GPIO_Init(LCD_RS_PORT, LCD_RS_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+GPIO_Init(LCD_RW_PORT, LCD_RW_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+GPIO_Init(LCD_E_PORT, LCD_E_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+GPIO_Init(LCD_D4_PORT, LCD_D4_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+GPIO_Init(LCD_D5_PORT, LCD_D5_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+GPIO_Init(LCD_D6_PORT, LCD_D6_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+GPIO_Init(LCD_D7_PORT, LCD_D7_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+GPIO_Init(swt_PORT,swt_PIN,GPIO_MODE_IN_FL_NO_IT);
+GPIO_Init (NCODER_CLK_PORT, NCODER_CLK_PIN, GPIO_MODE_IN_FL_NO_IT );
+GPIO_Init (NCODER_DATA_PORT, NCODER_DATA_PIN, GPIO_MODE_IN_FL_NO_IT );
+}
+
+//Proměnné
+char text[32];
+uint16_t enc_value = 0;
+_Bool minuly = 0;
+uint32_t time_enc = 0;
+/*
+uint32_t time_enc_swt = 0;
+uint32_t time_ye = 0;
+uint32_t time_run = 0;
+uint32_t time_error = 0;
+uint16_t akt = 0;
+int16_t max = 0;
+_Bool aktualni_stav = 0;
+_Bool minuly_stav = 0;
+_Bool run = 0;
+_Bool porucha = 0;
+*/
+
+int8_t check_ncoder(void){ //funkce pro ncoder
+int minuly = 0;
+if (minuly == 0 && NCODER_GET_CLK == 1){
+minuly = 1;
+if (NCODER_GET_DATA == 0){
+return 1;
+}else{
+return -1;
+}
+}else if (minuly == 1 && NCODER_GET_CLK ==0){
+minuly = 0;
+if (NCODER_GET_DATA ==0){
+return -1;
+}else{
+return 1;
+}
+}
+return 0;
+}
+
+void main(void){
+setup();
+lcd_init();
+init_milis();
+lcd_clear();
+
+while(1){
+   /*
+    if (enc_value < 0){ //Ošetření přetečení
+    enc_value=0;
+    }
+    else if (enc_value > 100){
+   enc_value=100;
+    }
+*/
+    if (milis() - time_enc > 7){
+    time_enc = milis();
+    enc_value += check_ncoder(); //Kontrola ncoderu
+    if ( enc_value < 0){ //Ošetření přetečení
+     enc_value=99;
+    }else if ( enc_value > 99){
+     enc_value=0;
+    }
+    }
+    /*
+    if (enc_swt == 0){
+    lcd_gotoxy(0,0);
+    lcd_puts("Vyberte tlacitko");
+    }
+    else {
+    */
+    /*
+    lcd_gotoxy(0,0);
+    sprintf(text, "0%10u");
+    lcd_puts(text);
+*/
+    lcd_gotoxy(0,0);
+   sprintf(text,"hodnota: %5d", enc_value);
+    lcd_puts(text);
+
+/*
+if (milis() - time_ncoder > 7){
+if (run==0){
+time_ncoder = milis();
+max += check_ncoder(); //Kontrola ncoderu
+if (max < 0){ //Ošetření přetečení
+max=99;
+}else if (max > 99){
+max=0;
+}
+}
+}
+if (milis() - time_enc_swt > 90){
+time_enc_swt=milis();
+if (enc_swt){ //Kontrola tl. na ncoderu
+aktualni_stav=1;
+}else{
+aktualni_stav=0;
+}
+if (minuly_stav==1 && aktualni_stav==0){
+minuly_stav=0;
+if (run==0){ //Spuštení programu
+run=1;
+}else if (run==1){ //Vypnutí programu
+run=0;
+
+}
+}
+minuly_stav=aktualni_stav;
+}
+}
+
+if (milis() - time_run > 150){
+time_run=milis();
+if (run==1 && porucha==0){ // zobrazení displeje při chodu programu
+lcd_clear();
+sprintf(text,"Max.:%3u",max);
+lcd_puts(text);
+}else if(run==0){ //zobraní displeje při stop módu
+lcd_gotoxy(8,0);
+sprintf(text,"Max.:%3u",max);
+}
+}
+
+if (TL==3){
+lcd_clear();
+lcd_gotoxy(0,0);
+sprintf(Text_TL_A,"f: 20Hz TH: %ums",Time_TL3_H);
+lcd_puts(Text_TL_A);
+lcd_gotoxy(8,1);
+sprintf(Text_TL_B,"TL: %ums" ,Time_TL3_L);
+lcd_puts(Text_TL_B);
 }
 
 
-#define L_PATTERN 0b01110000    // 3x125ns (8MHZ SPI)
-#define H_PATTERN 0b01111100    // 5x125ns (8MHZ SPI), first and last bit must be zero (to remain MOSI in Low between frames/bits)
-// takes array of LED_number * 3 bytes (RGB per LED)
+//if (milis() - time_ye > 190){
+//time_ye=milis();
+//if (run==0 && porucha==0){ //Při stop módu rozsvítit žlutou LED
+//}
 
-void neopixel(uint8_t * data, uint16_t length)
-{
-    uint8_t mask;
-    disableInterrupts();        // can be omitted if interrupts do not take more then about ~25us
-    while(length--) {            // for all bytes from input array
-        mask = 0b10000000;     // for all bits in byte
-        while (mask) {
-            while (!(SPI->SR & SPI_SR_TXE));    // wait for empty SPI buffer
-            if (mask & data[length]) {  // send pulse with coresponding length ("L" od "H")
-                SPI->DR = H_PATTERN;
-            } else {
-                SPI->DR = L_PATTERN;
-            }
-            mask = mask >> 1;
-        }
-    }
-    enableInterrupts();
-    while (SPI->SR & SPI_SR_BSY); // wait until end of transfer - there should come "reset" (>50us in Low)
+*/
 }
-
-
-// test pattern for (16 RGB LED ring)
-uint8_t colors[24] = {
-    1, 1, 1,     
-    1, 1, 1,    
-    0, 1, 1,   
-    0, 0, 0,    
-    0, 0, 0,   
-    0, 0, 0, 
-    0, 0, 0,         
-    0, 0, 0
-};
-
-void delay_ms(uint16_t ms) {
-    uint16_t  i;
-    for (i=0; i<ms; i = i+1){
-        _delay_us(250);
-        _delay_us(248);
-        _delay_us(250);
-        _delay_us(250);
-    }
 }
-
-int main(void)
-{
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
-    init_spi();
-
-        neopixel(colors, sizeof(colors));
-        delay_ms(2);
-
-    while (1) {
-        colors[4]=colors[4]+20;
-        if   (colors[4]>=235){
-            colors[4]=0;
-        }
-        delay_ms(100);
-        neopixel(colors, sizeof(colors));
-    }
- }
-
